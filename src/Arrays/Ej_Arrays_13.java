be52748b57e4dd3a92b791e3b16ecc050e7d38dc
sergi_aguilarruiz <sergi_aguilarruiz@iescarlesvallbona.cat>
2020-11-06 12:37:21 +0100

    d

--- src/Arrays/Ej_Arrays_13.java
+++ src/Arrays/Ej_Arrays_13.java
@@ -1,2 +1,22 @@
-package Arrays;public class Ej_Arrays_13 {
+package Arrays;
+
+public class Ej_Arrays_13 {
+    public static void main(String[] args) {
+        int[] array = new int[3];
+        for (int i = 0; i < array.length ; i++) {
+            array[i]=(int)(Math.random()*5);
+            System.out.println(array[i]);
+
+        }
+        int bak = array[array.length-1];
+        for (int i = array.length-1; i >0 ; i--) {
+            array[i]=array[i-1];
+
+        }
+        array[0]=bak;
+        for (int recorrer:array) {
+            System.out.println(recorrer);
+
+        }
+    }
 }
