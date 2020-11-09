88ae18ae051da66c10d3eee54be8093175f1e4e4
sergi_aguilarruiz <sergi_aguilarruiz@iescarlesvallbona.cat>
2020-11-09 13:51:33 +0100

    array ordenda

---
+++ src/S/Droid.java
@@ -0,0 +1,18 @@
+package S;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public Calculator(){
+
+    }
+    public int add(int a,int b){
+        System.out.println("The sume is ");
+        return a+b;
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
+
